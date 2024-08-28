3cdcf2fec8be2690527af0e4a464f70e76c1b3d0
kishita <kishita@gmail.com>
2024-08-28 14:35:14 +0530

    Add printGreeting method to Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        printGreeting();
+    }
+
+    public static void printGreeting() {
+        System.out.println("Welcome to the Java program!");
     }
 }
